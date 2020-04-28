75dd745be15ec3dc7bb2ce795a6a2852b80d3568
CristoferQ <ceql@live.cl>
2020-04-28 01:55:44 -0400

    wip check job

--- view/home/js/jobs/createJob.js
+++ view/home/js/jobs/createJob.js
@@ -69,39 +69,24 @@ $(document).ready(function() {
         $('#loading').hide();
 
         if (response.file == "ERROR"){
-          alert("Se ha producido un error en la subida de archivos");
-          location.href="";
+          var message = "Error con el archivo";
+          $("#error_text").html( message);
+          $('#errorResponse').show();
+          setTimeout("location.href=''", 5000);
         }
         else{
           if (response.exec == "ok"){
+            var message = "Correcto";
+            $("#success_text").html( message);
+            $('#okResponse').show();
             location.href="../checkJob/?user="+response.user+"&job="+response.job;
           }else{
-            alert("Se ha producido un error");
-            location.href="";
-          }
-          /*var nameFile = "../../../jobs/"+response.user+"/"+response.job+"/results/summary_log.json";
-          readTextFile(nameFile, function(text){
-            var data = JSON.parse(text);
-            console.log(data);
-
-            if (data.exec == 0){
-
-            }
-          
-            //$(".precisionDefinition").html(data.definitionPerformanceCla[0].definition);
-          });*/
-          /*if (response.exec == "ERROR"){
-            
-            var message = "Error during the creation of the job, please check the data set. If the error persists, please contact the administrator.";
-            $(".messageError").html( message);
-  
+            var message = "Se ha producido un error";
+            $("#error_text").html( message);
             $('#errorResponse').show();
             setTimeout("location.href=''", 5000);
-          }else{
-            var job = response.job;
-            //location.href="../checkJob/index.html";
           }
-          */
+          
         }
       });
     });
